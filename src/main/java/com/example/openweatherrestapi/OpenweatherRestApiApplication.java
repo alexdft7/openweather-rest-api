02261878fde719ff2b31267cb5bf91f4d5f8d569
alexdft7 <64974493+alexdft7@users.noreply.github.com>
2020-05-10 18:12:01 +0300

    Add files via upload

---
+++ src/main/java/com/example/openweatherrestapi/OpenweatherRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.openweatherrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenweatherRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenweatherRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/openweatherrestapi/OpenweatherRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.openweatherrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenweatherRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
