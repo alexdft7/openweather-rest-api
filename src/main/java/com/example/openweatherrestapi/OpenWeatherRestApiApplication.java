eb3314871b16ca1e044877b5dbe123f16f6ca1c0
alexdft7 <64974493+alexdft7@users.noreply.github.com>
2020-05-11 14:52:02 +0300

    Update and rename OpenweatherRestApiApplication.java to OpenWeatherRestApiApplication.java

--- src/main/java/com/example/openweatherrestapi/OpenWeatherRestApiApplication.java
+++ src/main/java/com/example/openweatherrestapi/OpenWeatherRestApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OpenweatherRestApiApplication {
+public class OpenWeatherRestApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OpenweatherRestApiApplication.class, args);
+		SpringApplication.run(OpenWeatherRestApiApplication.class, args);
 	}
 
 }
